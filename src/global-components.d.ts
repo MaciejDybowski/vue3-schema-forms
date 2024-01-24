2fd0f474de59b26baad31191503034a4255f0703
Maciej <maciej.dybowski@tecna.pl>
2024-01-24 07:37:21 +0100

    [fix] - add types to global-components.d.ts

--- src/global-components.d.ts
+++ src/global-components.d.ts
@@ -1,8 +1,10 @@
 import components from "./components";
 import "@vue/runtime-core";
+import { FormErrorVisualization } from '@/components';
 
 declare module "@vue/runtime-core" {
   export interface GlobalComponents {
     VueSchemaForms: typeof components.VueSchemaForms;
+    FormErrorVisualization: typeof components.FormErrorVisualization
   }
 }
