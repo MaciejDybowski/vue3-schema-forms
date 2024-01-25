af992e34dfe4fc7f6ed2c7a81512956ab1769a80
Maciej <maciej.dybowski@tecna.pl>
2024-01-25 08:05:58 +0100

    [fix] - update condition for offset and fillRow column

--- src/components/engine/FormNode.vue
+++ src/components/engine/FormNode.vue
@@ -1,30 +1,30 @@
 <template>
   <v-col
-    v-if="isOffsetExist"
-    :cols="offset"
+    v-if='isOffsetExist && shouldRender'
+    :cols='offset'
   ></v-col>
 
   <v-col
-    v-if="shouldRender"
-    :cols="cols"
-    v-show="hideField"
+    v-if='shouldRender'
+    :cols='cols'
+    v-show='hideField'
   >
     <component
-      :is="`node-${schema.layout.component}`"
-      :schema="schema"
-      :model="model"
+      :is='`node-${schema.layout.component}`'
+      :schema='schema'
+      :model='model'
     />
   </v-col>
   <v-col
-    v-if="fillRow"
-    :cols="completionOfRow"
+    v-if='fillRow && shouldRender'
+    :cols='completionOfRow'
   ></v-col>
 </template>
 
-<script setup lang="ts">
-import { EngineField } from "../../vocabulary/engine";
-import { useConditionalRendering } from "../../core/composables/useConditionalRendering";
-import { useSchemaCols } from "../../core/composables/useSchemaCols";
+<script setup lang='ts'>
+import { EngineField } from '../../vocabulary/engine';
+import { useConditionalRendering } from '../../core/composables/useConditionalRendering';
+import { useSchemaCols } from '../../core/composables/useSchemaCols';
 
 const props = defineProps<{
   schema: EngineField;
@@ -33,11 +33,12 @@ const props = defineProps<{
 
 const { shouldRender } = useConditionalRendering(props.schema);
 const { cols, completionOfRow, isOffsetExist, offset, fillRow, hideField } = useSchemaCols(props.schema);
+console.debug(props.schema.key, cols.value, completionOfRow.value, fillRow.value, shouldRender.value);
 </script>
 
-<style scoped lang="css"></style>
+<style scoped lang='css'></style>
 
-<i18n lang="json">
+<i18n lang='json'>
 {
   "en": {},
   "pl": {}
