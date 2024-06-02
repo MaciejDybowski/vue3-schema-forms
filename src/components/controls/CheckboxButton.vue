d727c85bcb0798bc12209c7647efb51a75a90e72
Maciej <maciej.dybowski@tecna.pl>
2024-06-02 22:09:09 +0200

    Add option multiple for checkbox and add new script

--- src/components/controls/CheckboxButton.vue
+++ src/components/controls/CheckboxButton.vue
@@ -1,35 +1,35 @@
 <template>
   <div
     :class="'checkbox-root ' + bindClass(schema)"
-    v-if="!loading"
+    v-if='!loading'
   >
-    <label class="v-label">
+    <label v-if='label' class='v-label'>
       {{ label }}
     </label>
     <template
-      v-for="(option, index) in data"
-      :key="option[value]"
+      v-for='(option, index) in data'
+      :key='option[value]'
     >
       <v-checkbox
-        v-model="localModel"
-        v-bind="bindProps(schema)"
-        :rules="rules(schema)"
-        :label="option[title]"
-        :value="option[value]"
+        v-model='localModel'
+        v-bind='bindProps(schema)'
+        :rules='rules(schema)'
+        :label='option[title]'
+        :value='option[value]'
         :hide-details="index == data.length - 1 ? 'auto' : true"
       >
-        <template #message="{ message }">
-          <div class="ml-4">{{ message }}</div>
+        <template #message='{ message }'>
+          <div class='ml-4'>{{ message }}</div>
         </template>
       </v-checkbox>
     </template>
   </div>
 </template>
 
-<script setup lang="ts">
-import { EngineSourceField } from "../../vocabulary/engine/controls";
-import { computed } from "vue";
-import { useClass, useFormModel, useLabel, useProps, useRules, useSource } from "../../core/composables";
+<script setup lang='ts'>
+import { EngineSourceField } from '../../vocabulary/engine/controls';
+import { computed } from 'vue';
+import { useClass, useFormModel, useLabel, useProps, useRules, useSource } from '../../core/composables';
 
 const props = defineProps<{
   schema: EngineSourceField;
@@ -43,6 +43,8 @@ const { bindProps } = useProps();
 const { bindClass } = useClass();
 const { getValue, setValue } = useFormModel();
 
+const cProps = bindProps(props.schema);
+
 const localModel = computed({
   get(): string | number {
     if (returnObject) {
@@ -54,12 +56,20 @@ const localModel = computed({
   set(val: any) {
     if (returnObject) {
       const arrayOfObj = data.value.filter((obj) => val?.includes(obj[value])).map((item) => item);
-      setValue(arrayOfObj.length > 0 ? arrayOfObj : null, props.schema);
+      if (!cProps.multiple) {
+        setValue(arrayOfObj.length > 0 ? arrayOfObj[0] : null, props.schema);
+      } else {
+        setValue(arrayOfObj.length > 0 ? arrayOfObj : null, props.schema);
+      }
     } else {
-      setValue(val.length > 0 ? val : null, props.schema);
+      if (!cProps.multiple) {
+        setValue(val, props.schema);
+      } else {
+        setValue(val.length > 0 ? val : null, props.schema);
+      }
     }
   },
 });
 </script>
 
-<style scoped lang="css"></style>
+<style scoped lang='css'></style>
