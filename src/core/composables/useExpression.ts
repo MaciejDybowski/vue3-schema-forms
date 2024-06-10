8ba73381203830c5cda77cf2cf54fdf879f136e5
maciek dybowski <maciej.dybowski@tecna.pl>
2024-06-10 09:37:43 +0200

    Dev Expression Functions

--- src/core/composables/useExpression.ts
+++ src/core/composables/useExpression.ts
@@ -1,29 +1,30 @@
 import { ref, watch } from "vue";
-import {
-  MIN,
-  MAX,
-  CALC_DATE_DIFF_RETURN_DAY,
-  CALC_DATE_DIFF_RETURN_HOURS, CALC_DATE_DIFF_RETURN_MINUTES
-} from "../engine/evalExprParser";
 import set from "lodash/set";
+import {functions} from "../engine/expressionResolver"
 
 export function useExpression(key: string, expression: string, model: object){
 
   let result = ref();
-  result.value = MIN(result.value ? result.value :expression, model);
-  result.value = MAX(result.value ? result.value :expression, model)
-  result.value = CALC_DATE_DIFF_RETURN_DAY(result.value ? result.value :expression, model)
-  result.value = CALC_DATE_DIFF_RETURN_HOURS(result.value ? result.value :expression, model)
-  result.value = CALC_DATE_DIFF_RETURN_MINUTES(result.value ? result.value :expression, model)
+  let f = functions[extractFunctionName(expression)]
+  result.value = f(expression, model)
 
   watch(model, () => {
-    result.value = MIN(result.value ? result.value :expression, model);
-    result.value = MAX(result.value ? result.value :expression, model)
-    result.value = CALC_DATE_DIFF_RETURN_HOURS(result.value ? result.value :expression, model)
-    result.value = CALC_DATE_DIFF_RETURN_HOURS(result.value ? result.value :expression, model)
-    result.value = CALC_DATE_DIFF_RETURN_MINUTES(result.value ? result.value :expression, model)
+    result.value = f(expression, model)
     set(model, key, result.value)
   });
 
   return result.value;
+}
+
+function extractFunctionName(expression: string): string{
+  try {
+    const match = expression.match(/^(.*?)\(/);
+    if (match) {
+      return match[1].trim();
+    }
+  } catch (error){
+   console.error(error)
+  } finally {
+    return ""
+  }
 }
