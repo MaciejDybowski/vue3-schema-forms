7977eaff99472ed524d6430e3f2f0e28752bcada
Maciej <maciej.dybowski@tecna.pl>
2024-09-11 19:36:29 +0200

    [revert] - back to default null if not defined

--- src/core/composables/useResolveVariables.ts
+++ src/core/composables/useResolveVariables.ts
@@ -1,62 +1,48 @@
-import get from "lodash/get";
-import { useI18n } from "vue-i18n";
+import get from 'lodash/get';
 
-import dayjs from "@/components/controls/date/dayjs";
+import dayjs from '@/components/controls/date/dayjs';
 
-import { EngineField } from "@/types/engine/EngineField";
+import { EngineField } from '@/types/engine/EngineField';
 
-import { useFormattedNumber } from "../../core/composables";
-import { variableRegexp } from "../engine/utils";
-import { useDateFormat } from "./useDateFormat";
-import { usePreparedModelForExpression } from "@/core/composables/usePreparedModelForExpression";
+import { useFormattedNumber } from '../../core/composables';
+import { variableRegexp } from '../engine/utils';
+import { useDateFormat } from './useDateFormat';
+import { usePreparedModelForExpression } from '@/core/composables/usePreparedModelForExpression';
 
 export function useResolveVariables(field: EngineField) {
   const { formatNumber } = useFormattedNumber(field.options);
   const { dateFormat } = useDateFormat();
-  const { t, messages, locale } = useI18n();
-  switch (locale.value) {
-    case "pl":
-      if (messages.value["pl"] == undefined) messages.value["pl"] = {};
-      messages.value["pl"]["noData"] = "Brak danych";
-      break;
-    case "en":
-      if (messages.value["en"] == undefined) messages.value["en"] = {};
-      messages.value["en"]["noData"] = "No value";
-      break;
-    default:
-      if (messages.value["en"] == undefined) messages.value["en"] = {};
-      messages.value["en"]["noData"] = "No value";
-  }
 
-  function resolve(inputString: string, title: string = "title") {
+
+  function resolve(inputString: string, title: string = 'title') {
     let allVariablesResolved = true;
 
     inputString?.match(variableRegexp)?.forEach((match: string) => {
       const unwrapped = match.slice(1, -1);
-      const split = unwrapped.split(":");
+      const split = unwrapped.split(':');
       const variable = split[0];
 
-      const defaultValue = split.length === 2 ? split[1] : t("noData");
+      const defaultValue = split.length === 2 ? split[1] : null;
 
       const model = usePreparedModelForExpression(field);
 
       let value = get(model, variable, defaultValue);
 
-      if (typeof value === "number") {
+      if (typeof value === 'number') {
         value = formatNumber(value);
       }
-      if (typeof value === "string" && dayjs(value).isValid() && value.length == 10
-        && (value.includes("/") || value.includes(".") || value.includes("-"))) {
+      if (typeof value === 'string' && dayjs(value).isValid() && value.length == 10
+        && (value.includes('/') || value.includes('.') || value.includes('-'))) {
         value = dayjs(value).format(dateFormat.value);
       }
-      if (typeof value == "object" && value !== null) {
+      if (typeof value == 'object' && value !== null) {
         value = value[title];
       }
 
       if (!value) {
         allVariablesResolved = false;
       }
-      inputString = inputString.replace(match, value + "");
+      inputString = inputString.replace(match, value + '');
     });
 
     return { resolvedText: inputString, allVariablesResolved };
