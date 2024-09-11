6b5aae6f38c66fcdca7d6efb93cd6d8a466c4187
Maciej <maciej.dybowski@tecna.pl>
2024-09-11 19:36:03 +0200

    [dev] - add json nata

--- src/core/composables/useConditionalRendering.ts
+++ src/core/composables/useConditionalRendering.ts
@@ -1,12 +1,13 @@
-import { Expression, Value } from "expr-eval";
-import get from "lodash/get";
-import { ref } from "vue";
+import { Expression, Value } from 'expr-eval';
+import get from 'lodash/get';
+import { ref } from 'vue';
 
-import { usePreparedModelForExpression } from "@/core/composables/usePreparedModelForExpression";
-import { EngineField } from "@/types/engine/EngineField";
+import { usePreparedModelForExpression } from '@/core/composables/usePreparedModelForExpression';
+import { EngineField } from '@/types/engine/EngineField';
 
-import { useFormModelStore } from "../../store/formModelStore";
-import betterParser from "../engine/evalExprParser";
+import { useFormModelStore } from '../../store/formModelStore';
+import betterParser from '../engine/evalExprParser';
+import jsonata from 'jsonata';
 
 export function useConditionalRendering(schema: EngineField) {
   const formModelStore = useFormModelStore(schema.formId);
@@ -15,6 +16,14 @@ export function useConditionalRendering(schema: EngineField) {
   let model = {} as unknown;
 
   if (!shouldRender.value) {
+    if (schema.layout.if?.includes('nata-')) {
+      ifByJsonNata(schema.layout.if.replace('nata-', ''));
+    } else {
+      ifByEvalExpression();
+    }
+  }
+
+  function ifByEvalExpression() {
     let myExpr: Expression = betterParser.parse(schema.layout.if as string);
     model = usePreparedModelForExpression(schema);
     if (myExpr.variables({ withMembers: true }).every((variable) => get(model, variable, null) !== null)) {
@@ -26,6 +35,18 @@ export function useConditionalRendering(schema: EngineField) {
       if (myExpr.variables({ withMembers: true }).every((variable) => get(model, variable, null) !== null)) {
         shouldRender.value = myExpr.evaluate(model as Value);
       }
+    });
+  }
+
+  async function ifByJsonNata(expression: string) {
+    const nata = jsonata(expression);
+    model = usePreparedModelForExpression(schema);
+    shouldRender.value = await nata.evaluate(model);
+    console.debug(shouldRender.value)
+
+    formModelStore.$subscribe(async () => {
+      model = usePreparedModelForExpression(schema);
+      shouldRender.value = await nata.evaluate(model);
     });
   }
 
