30e4f213b8f55414b5d3a6a706fd313a7a1e38da
Maciej Dybowski <maciej.dybowski@tecna.pl>
2024-10-28 16:59:27 +0100

    [fix] - oridinal number on copy mode

--- src/components/controls/duplicated-section/DuplicatedSection.vue
+++ src/components/controls/duplicated-section/DuplicatedSection.vue
@@ -1,34 +1,34 @@
 <template>
   <v-row>
-    <v-col cols='12'>
+    <v-col cols="12">
       <draggable
-        v-model='nodes'
-        group='sections'
-        @start='drag = true'
-        @end='drag = false'
-        @change='changePosition'
-        handle='.draggable-icon'
-        item-key='id'
-        v-bind='dragOptions'
+        v-model="nodes"
+        group="sections"
+        handle=".draggable-icon"
+        item-key="id"
+        v-bind="dragOptions"
+        @change="changePosition"
+        @end="drag = false"
+        @start="drag = true"
       >
-        <template #item='{ element, index }'>
-          <duplicated-section-item :show-divider='isShowDivider && index <= nodes.length - 2'>
-            <template #box='{ isHovering }'>
+        <template #item="{ element, index }">
+          <duplicated-section-item :show-divider="isShowDivider && index <= nodes.length - 2">
+            <template #box="{ isHovering }">
               <draggable-icon
-                v-if='isEditable && showSectionElements'
-                :show='isHovering ? isHovering : false'
+                v-if="isEditable && showSectionElements"
+                :show="isHovering ? isHovering : false"
               />
               <form-root
-                :model='localModel[index]'
-                @update:model='updateModel($event, index)'
-                :options='computedOptions'
-                :schema='element as Schema'
-                :form-id='schema.formId'
+                :form-id="schema.formId"
+                :model="localModel[index]"
+                :options="computedOptions"
+                :schema="element as Schema"
+                @update:model="updateModel($event, index)"
               />
               <draggable-context-menu
-                v-if='isEditable && showSectionElements'
-                :show='isHovering ? isHovering : false'
-                @handle-action='handleDraggableContextAction($event, index)'
+                v-if="isEditable && showSectionElements"
+                :show="isHovering ? isHovering : false"
+                @handle-action="handleDraggableContextAction($event, index)"
               />
             </template>
           </duplicated-section-item>
@@ -37,11 +37,11 @@
     </v-col>
     <v-col>
       <v-btn
-        v-if='isEditable && showSectionElements'
-        prepend-icon='mdi-plus'
-        color='primary'
-        v-bind='schema.options.buttonProps'
-        @click='runDuplicatedSectionButtonLogic'
+        v-if="isEditable && showSectionElements"
+        color="primary"
+        prepend-icon="mdi-plus"
+        v-bind="schema.options.buttonProps"
+        @click="runDuplicatedSectionButtonLogic"
       >
         {{ getAddBtnText }}
       </v-btn>
@@ -49,27 +49,28 @@
   </v-row>
 </template>
 
-<script setup lang='ts'>
+<script lang="ts" setup>
 import { cloneDeep, isArray } from "lodash";
-import get from 'lodash/get';
-import set from 'lodash/set';
-import { v4 as uuidv4 } from 'uuid';
-import { computed, onMounted, ref } from 'vue';
-import { useI18n } from 'vue-i18n';
-import draggable from 'vuedraggable';
+import get from "lodash/get";
+import set from "lodash/set";
+import { v4 as uuidv4 } from "uuid";
+import { computed, onMounted, ref } from "vue";
+import { useI18n } from "vue-i18n";
+import draggable from "vuedraggable";
 
-import { useFormModel } from '@/core/composables';
-import { VueDragable } from '@/types/VueDragable';
-import { NodeUpdateEvent } from '@/types/engine/NodeUpdateEvent';
-import { EngineDuplicatedSection } from '@/types/engine/controls';
-import { Schema } from '@/types/schema/Schema';
-import { SchemaField } from '@/types/schema/SchemaField';
-import { DuplicatedSectionOptions } from '@/types/shared/DuplicatedSectionOptions';
+import { useFormModel } from "@/core/composables";
+import { VueDragable } from "@/types/VueDragable";
+import { NodeUpdateEvent } from "@/types/engine/NodeUpdateEvent";
+import { EngineDuplicatedSection } from "@/types/engine/controls";
+import { Schema } from "@/types/schema/Schema";
+import { SchemaField } from "@/types/schema/SchemaField";
+import { DuplicatedSectionOptions } from "@/types/shared/DuplicatedSectionOptions";
 
-import FormRoot from '../../engine/FormRoot.vue';
-import DraggableContextMenu from './DraggableContextMenu.vue';
-import DraggableIcon from './DraggableIcon.vue';
-import DuplicatedSectionItem from './DuplicatedSectionItem.vue';
+import FormRoot from "../../engine/FormRoot.vue";
+import DraggableContextMenu from "./DraggableContextMenu.vue";
+import DraggableIcon from "./DraggableIcon.vue";
+import DuplicatedSectionItem from "./DuplicatedSectionItem.vue";
+import { addons } from "@storybook/manager-api";
 
 const props = defineProps<{
   schema: EngineDuplicatedSection;
@@ -83,34 +84,35 @@ const { t } = useI18n();
 const dragOptions = ref({
   animation: 200,
   disabled: false,
-  ghostClass: 'ghost',
+  ghostClass: "ghost",
 });
-
 
 const duplicatedSectionOptions = ref(props.schema.layout?.options as DuplicatedSectionOptions);
 
 const { getValue, setValue } = useFormModel();
 
-const isEditable: boolean = 'editable' in props.schema ? (props.schema.editable as boolean) : true;
+const isEditable: boolean = "editable" in props.schema ? (props.schema.editable as boolean) : true;
 const showSectionElements = computed(() => {
   if (isEditable) {
-    return 'showElements' in props.schema ? (props.schema.showElements as boolean) : true;
+    return "showElements" in props.schema ? (props.schema.showElements as boolean) : true;
   } else {
     return false;
   }
 });
 
-const ordinalNumberInModel: boolean = duplicatedSectionOptions.value !== undefined &&
-'ordinalNumberInModel' in duplicatedSectionOptions.value ? duplicatedSectionOptions.value.ordinalNumberInModel : false;
+const ordinalNumberInModel: boolean =
+  duplicatedSectionOptions.value !== undefined && "ordinalNumberInModel" in duplicatedSectionOptions.value
+    ? duplicatedSectionOptions.value.ordinalNumberInModel
+    : false;
 
 const computedOptions = computed(() => {
   const options = JSON.parse(JSON.stringify(props.schema.options));
 
   if (!isEditable) {
-    if (!('fieldProps' in options)) {
+    if (!("fieldProps" in options)) {
       options.fieldProps = {};
     }
-    options.fieldProps['readonly'] = true;
+    options.fieldProps["readonly"] = true;
   }
   return options;
 });
@@ -120,24 +122,26 @@ function updateModel(event: NodeUpdateEvent, indexOfArray: number) {
   setValue(localModel.value, props.schema, indexOfArray);
 }
 
-function handleDraggableContextAction(actionId: 'delete' | 'addBelow' | string, index: number) {
+function handleDraggableContextAction(actionId: "delete" | "addBelow" | string, index: number) {
   switch (actionId) {
-    case 'addBelow':
+    case "addBelow":
       nodes.value.splice(index + 1, 0, getClearNode.value);
       localModel.value.splice(index + 1, 0, getClearModel.value);
       setValue(localModel.value, props.schema, index);
       return;
-    case 'delete':
-      nodes.value = nodes.value.filter((item, i) => i !== index)
+    case "delete":
+      nodes.value = nodes.value
+        .filter((item, i) => i !== index)
         .map((item, index) => {
           wrapPropertiesWithIndexAndPath(item.properties, index);
           return item;
         });
 
-      localModel.value = localModel.value.filter((item, i) => i !== index)
+      localModel.value = localModel.value
+        .filter((item, i) => i !== index)
         .map((item, index) => {
           if (ordinalNumberInModel) {
-            item['ordinalNumber'] = index + 1;
+            item["ordinalNumber"] = index + 1;
           }
           return item;
         });
@@ -145,19 +149,27 @@ function handleDraggableContextAction(actionId: 'delete' | 'addBelow' | string, 
       return;
     case "copyBelow":
       nodes.value.splice(index + 1, 0, getClearNode.value);
-      const copiedModel = ref(cloneDeep(localModel.value[index]))
-      localModel.value.splice(index + 1, 0, copiedModel.value);
+      const copiedModel = ref(cloneDeep(localModel.value[index]));
+
+      if (ordinalNumberInModel) {
+        copiedModel.value["ordinalNumber"] = ++copiedModel.value["ordinalNumber"];
+        localModel.value.splice(index + 1, 0, copiedModel.value);
+        for (let i = index + 2; i < localModel.value.length; i++) {
+          localModel.value[i]["ordinalNumber"] = localModel.value[i]["ordinalNumber"] + 1;
+        }
+      }
+
       setValue(localModel.value, props.schema, index);
       return;
     default:
-      console.error('Unknown action');
+      console.error("Unknown action");
   }
 }
 
 const getClearNode = computed((): Schema => {
   return {
     id: uuidv4(),
-    type: 'object',
+    type: "object",
     properties: wrapPropertiesWithIndexAndPath(
       JSON.parse(JSON.stringify(props.schema.layout.schema?.properties)),
       nodes.value.length,
@@ -169,25 +181,35 @@ const getClearNode = computed((): Schema => {
 const getClearModel = computed(() => {
   const newLocalModel = {};
   if (ordinalNumberInModel) {
-    newLocalModel['ordinalNumber'] = nodes.value.length;
+    newLocalModel["ordinalNumber"] = nodes.value.length;
   }
   return newLocalModel;
 });
 
-function runDuplicatedSectionButtonLogic(): void {
-
-  if(getAddBtnMode.value == 'add'){
+function runDuplicatedSectionButtonLogic(init = false): void {
+  if (getAddBtnMode.value == "add") {
     nodes.value.push(getClearNode.value);
-    localModel.value.push({...getClearModel.value});
+    localModel.value.push({ ...getClearModel.value });
   }
-  if(getAddBtnMode.value == 'copy'){
+  if (getAddBtnMode.value == "copy") {
     nodes.value.push(getClearNode.value);
-    localModel.value.push({...localModel.value[localModel.value.length - 1]});
+    addNewModelToDuplicatedSectionWhenCopyBtnMode(init)
   }
-
 
   if (ordinalNumberInModel) {
     setValue(localModel.value, props.schema, nodes.value.length - 1);
+  }
+}
+
+function addNewModelToDuplicatedSectionWhenCopyBtnMode(init = false){
+  if (init) {
+    localModel.value.push({ ...getClearModel.value });
+  } else {
+    const copiedLastRowModel = { ...localModel.value[localModel.value.length - 1] };
+    if (ordinalNumberInModel) {
+      copiedLastRowModel["ordinalNumber"] = ++copiedLastRowModel["ordinalNumber"];
+    }
+    localModel.value.push(copiedLastRowModel);
   }
 }
 
@@ -198,18 +220,17 @@ function changePosition(drag: VueDragable<Schema>) {
     localModel.value[drag.moved.oldIndex] = temp;
 
     if (ordinalNumberInModel) {
-      const tempOrdinal = localModel.value[drag.moved.newIndex]['ordinalNumber'];
-      localModel.value[drag.moved.newIndex]['ordinalNumber'] = localModel.value[drag.moved.oldIndex]['ordinalNumber'];
-      localModel.value[drag.moved.oldIndex]['ordinalNumber'] = tempOrdinal;
+      const tempOrdinal = localModel.value[drag.moved.newIndex]["ordinalNumber"];
+      localModel.value[drag.moved.newIndex]["ordinalNumber"] = localModel.value[drag.moved.oldIndex]["ordinalNumber"];
+      localModel.value[drag.moved.oldIndex]["ordinalNumber"] = tempOrdinal;
     }
 
     nodes.value = nodes.value.map((item, index) => {
       wrapPropertiesWithIndexAndPath(item.properties, index);
       return item;
     });
-
   } else {
-    console.warn('Error with draggable');
+    console.warn("Error with draggable");
   }
 }
 
@@ -217,7 +238,7 @@ const getAddBtnText = computed(() => {
   if (duplicatedSectionOptions.value?.addBtnText) {
     return duplicatedSectionOptions.value.addBtnText;
   } else {
-    return t('addAction');
+    return t("addAction");
   }
 });
 
@@ -225,9 +246,9 @@ const getAddBtnMode = computed(() => {
   if (duplicatedSectionOptions.value?.addBtnMode) {
     return duplicatedSectionOptions.value.addBtnMode;
   } else {
-    return "add"
+    return "add";
   }
-})
+});
 
 const isShowDivider = computed(() => {
   if (duplicatedSectionOptions.value?.showDivider) {
@@ -249,7 +270,7 @@ function init(): void {
     sections.forEach((item: any, index: number) => {
       nodes.value.push({
         id: uuidv4(),
-        type: 'object',
+        type: "object",
         properties: isDefaultExist
           ? mapPropertiesIfDefault(props.schema.layout.schema?.properties as Record<any, SchemaField>, sections[index])
           : wrapPropertiesWithIndexAndPath(JSON.parse(JSON.stringify(props.schema.layout.schema?.properties)), index),
@@ -258,17 +279,17 @@ function init(): void {
       localModel.value.push(isDefaultExist ? {} : sections[index]);
     });
   } else {
-    runDuplicatedSectionButtonLogic();
+    runDuplicatedSectionButtonLogic(true);
   }
 }
 
 function wrapPropertiesWithIndexAndPath(properties: Record<string, SchemaField>, index: number): Record<string, SchemaField> {
   for (let [key, value] of Object.entries(properties)) {
-    if ('properties' in value) {
+    if ("properties" in value) {
       wrapPropertiesWithIndexAndPath(value.properties as any, index);
     } else {
-      value['path'] = props.schema.key;
-      value['index'] = index;
+      value["path"] = props.schema.key;
+      value["index"] = index;
     }
   }
 
@@ -288,14 +309,14 @@ onMounted(() => {
 });
 </script>
 
-<style scoped lang='scss'>
+<style lang="scss" scoped>
 .ghost {
   opacity: 0.5;
   background: #d3d3d3;
 }
 </style>
 
-<i18n lang='json'>
+<i18n lang="json">
 {
   "en": {
     "addAction": "Add"
