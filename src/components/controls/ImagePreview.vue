a3d3069db5a48e53dbc3d331baff62817366dbea
Maciej Dybowski <maciej.dybowski@tecna.pl>
2024-11-02 12:43:45 +0100

    [dev] - better image-preview behaviour

--- src/components/controls/ImagePreview.vue
+++ src/components/controls/ImagePreview.vue
@@ -2,6 +2,7 @@
   <v-dialog max-width="500">
     <template v-slot:activator="{ props: activatorProps }">
       <v-avatar
+        v-if="resolvedThumbnail.resolvedText && resolvedThumbnail.allVariablesResolved && !image404Error"
         size="48"
         style="cursor: pointer; border: 0"
         v-bind="{ ...fieldProps, ...activatorProps }"
@@ -9,13 +10,26 @@
         <v-img
           :src="resolvedThumbnail.resolvedText"
           alt="thumbnail"
+          @error="handleImageError"
         />
       </v-avatar>
+      <v-tooltip v-else-if="image404Error" :text="t('imageNotFound', {src: resolvedThumbnail.resolvedText})">
+        <template v-slot:activator="{ props }">
+          <v-avatar
+            v-bind="props"
+            size="48"
+            color="warning"
+            icon="mdi-file-alert-outline"
+          />
+        </template>
+      </v-tooltip>
+      <v-skeleton-loader v-else type="avatar"/>
     </template>
     <template v-slot:default="{ isActive }">
       <v-card>
         <v-card-text>
           <v-img
+            v-if="resolvedPreview.resolvedText && resolvedPreview.allVariablesResolved"
             :src="resolvedPreview.resolvedText"
             alt="=preview"
             cover
@@ -37,7 +51,7 @@
 </template>
 
 <script lang="ts" setup>
-import { computed, onMounted } from "vue";
+import { computed, onMounted, ref } from "vue";
 import { useI18n } from "vue-i18n";
 import { useTheme } from "vuetify";
 
@@ -65,21 +79,32 @@ const resolvedPreview = computed(() => {
   return resolve(props.schema, props.schema.source.preview);
 });
 
+const image404Error = ref(false)
+
+function handleImageError() {
+  image404Error.value = true;
+}
+
 onMounted(() => {
   bindProps(props.schema);
 });
 </script>
 
 <style lang="css" scoped>
+>>> .v-skeleton-loader__avatar {
+  margin: 0 0 0 0;
+}
 </style>
 
 <i18n lang="json">
 {
   "en": {
-    "closeDialog": "Close"
+    "closeDialog": "Close",
+    "imageNotFound": "Image with src {src} not found."
   },
   "pl": {
-    "closeDialog": "Zamknij"
+    "closeDialog": "Zamknij",
+    "imageNotFound": "Zdjęcia o ścieżce {src} nie znaleziono."
   }
 }
 </i18n>
