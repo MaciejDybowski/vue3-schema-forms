c4bc57ff7d5ee38977aef21c7116b31d5467322a
Maciej Dybowski <maciej.dybowski@tecna.pl>
2024-12-05 17:49:06 +0100

    [fix] - rawNumber if exist

--- src/core/composables/useResolveVariables.ts
+++ src/core/composables/useResolveVariables.ts
@@ -1,26 +1,24 @@
-import get from 'lodash/get';
+import get from "lodash/get";
 
-import dayjs from '@/components/controls/date/dayjs';
+import dayjs from "@/components/controls/date/dayjs";
 
-import { useNumber } from '@/core/composables/useNumber';
-import { usePreparedModelForExpression } from '@/core/composables/usePreparedModelForExpression';
-import { EngineField } from '@/types/engine/EngineField';
+import { useNumber } from "@/core/composables/useNumber";
+import { usePreparedModelForExpression } from "@/core/composables/usePreparedModelForExpression";
+import { EngineField } from "@/types/engine/EngineField";
 
-import { variableRegexp } from '../engine/utils';
-import { useDateFormat } from './useDateFormat';
-import { useFormModelStore } from "@/store/formModelStore";
-import jsonata from "jsonata";
+import { variableRegexp } from "../engine/utils";
+import { useDateFormat } from "./useDateFormat";
 
 export function useResolveVariables() {
   const { dateFormat } = useDateFormat();
   const { formattedNumber, roundTo } = useNumber();
 
-  function resolve(field: EngineField, inputString: string, title: string = 'title', rawNumber= false) {
+  function resolve(field: EngineField, inputString: string, title: string = "title", rawNumber = false) {
     let allVariablesResolved = true;
 
-    inputString?.match(variableRegexp)?.forEach( (match: string) => {
+    inputString?.match(variableRegexp)?.forEach((match: string) => {
       const unwrapped = match.slice(1, -1);
-      const split = unwrapped.split(':');
+      const split = unwrapped.split(":");
       const variable = split[0];
 
       const defaultValue = split.length === 2 ? split[1] : null;
@@ -37,35 +35,38 @@ export function useResolveVariables() {
       let value = await nata.evaluate(model);
       */
 
-      if (typeof value === 'number' && value !== 0) {
-        if(rawNumber){
+      if (typeof value === "number" && value !== 0) {
+        if (rawNumber) {
           // gdy chcemy używać liczb w adresie URL to nie może być to kropka ani nie może być to formatowane
           // TODO
           value = Number(value);
-          value = value.replaceAll(",", ".")
+          if (value && !isNaN(value)) {
+            value = value + "";
+            value = value.replaceAll(",", ".");
+          }
         } else {
-          value = formattedNumber(value, 'decimal', field.precision ? Number(field.precision) : 2);
+          value = formattedNumber(value, "decimal", field.precision ? Number(field.precision) : 2);
         }
       }
       if (
-        typeof value === 'string' &&
+        typeof value === "string" &&
         dayjs(value).isValid() &&
         value.length == 10 &&
-        (value.includes('/') || value.includes('.') || value.includes('-'))
+        (value.includes("/") || value.includes(".") || value.includes("-"))
       ) {
         value = dayjs(value).format(dateFormat.value);
       }
-      if (typeof value == 'object' && value !== null) {
+      if (typeof value == "object" && value !== null) {
         value = value[title];
       }
 
-      if ((value == null && defaultValue !== null) || value == '') {
+      if ((value == null && defaultValue !== null) || value == "") {
         value = defaultValue;
       }
       if (!value) {
         allVariablesResolved = false;
       }
-      inputString = inputString.replace(match, value + '');
+      inputString = inputString.replace(match, value + "");
     });
 
     return { resolvedText: inputString, allVariablesResolved };
