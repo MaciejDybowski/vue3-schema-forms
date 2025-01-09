2410addf549b42ee1a720ad1a0803f6297b66f74
Maciej Dybowski <maciej.dybowski@tecna.pl>
2025-01-09 08:35:44 +0100

    [tests] - context object stories

---
+++ src/stories/features/ContextObject.stories.ts
@@ -0,0 +1,68 @@
+// @ts-nocheck
+import { VueSchemaForms } from "@/components";
+import { Meta, StoryObj } from "@storybook/vue3";
+
+const meta = {
+  title: "Forms/Features/Context object",
+  component: VueSchemaForms,
+  tags: ["autodocs"],
+  argTypes: {
+    schema: {
+      control: "object",
+      description: "Schema u" /*table: { disable: true }*/,
+    },
+    modelValue: {
+      control: "object",
+      description: "Model" /*table: { disable: true }*/,
+    },
+    options: {
+      control: "object",
+      description: "Opcje" /*table: { disable: true }*/,
+    },
+    "update:modelValue": { table: { disable: true } },
+  },
+  args: {
+    modelValue: {},
+    options: {},
+  },
+  parameters: {
+    controls: { hideNoControlsWarning: true }, //https://github.com/storybookjs/storybook/issues/24422
+  },
+} satisfies Meta<typeof VueSchemaForms>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Standard: Story = {
+  play: async (context) => {},
+  args: {
+    modelValue: {},
+    schema: {
+      properties: {
+        description: {
+          content: "We have access for `context` object in model for resolve variables. Context variables: userInfo, workspaceId, menuFeatureId",
+          layout: {
+            component: "static-content",
+            tag: "span",
+          },
+        },
+        item: {
+          label: "Field with context dependency = {context.userInfo.username}",
+          layout: {
+            component: "text-field",
+          },
+        },
+      },
+    },
+    options: {
+      context: {
+        userInfo: {
+          username: "Maciej",
+        },
+        workspaceId: "test",
+        menuFeatureId: "test"
+      },
+    },
+  },
+};
