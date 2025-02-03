a4b003e25e9bc94e307a6ea5bdf8b6d6a31d8c3e
Maciej Dybowski <maciej.dybowski@tecna.pl>
2025-02-03 21:19:57 +0100

    [dev] - field group update emit event on internal bus

--- src/components/controls/fields-group/FieldsGroup.vue
+++ src/components/controls/fields-group/FieldsGroup.vue
@@ -14,16 +14,25 @@ import { EngineTextField } from "@/types/engine/controls";
 import FormRoot from "@/components/engine/FormRoot.vue";
 import { onMounted, ref } from "vue";
 import set from "lodash/set";
+import { useFormModel } from "@/core/composables";
+import { useEventBus } from "@vueuse/core";
 
 const props = defineProps<{
   schema: EngineTextField;
   model: object;
 }>();
 
+const vueSchemaFormEventBus = useEventBus<string>("form-model");
+const { getValue, setValue } = useFormModel();
+
 const jsonSchemaOfGroup = ref<any>(null)
 
 function updateModel(payload: any){
   set(props.model, payload.key, payload.value);
+  vueSchemaFormEventBus.emit("model-changed", null);
+
+  // TODO jak zaktualizować pole wyżej skoro to tylko grupa wizualna, chyba trzeba to przerobic w properties ;)
+  //setValue(payload.value, props.schema)
 }
 
 onMounted(() => {
