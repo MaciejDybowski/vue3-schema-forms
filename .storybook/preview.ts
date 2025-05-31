1b9fc1f79fbf6b31599d33afea106fce937ca367
Maciej Dybowski <maciej.dybowski@tecna.pl>
2025-05-31 08:37:23 +0200

    [dev] - remove deps

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,12 +1,10 @@
-import { initialize, mswLoader } from "msw-storybook-addon";
+import type { Preview } from '@storybook/vue3-vite';
+import { setup } from '@storybook/vue3-vite';
+import { initialize, mswLoader } from 'msw-storybook-addon';
 
-import { install as VueMonacoEditorPlugin } from "@guolao/vue-monaco-editor";
-import type { Preview } from "@storybook/vue3-vite";
-import { setup } from "@storybook/vue3-vite";
-
-import i18n, { i18nConfig } from "./plugins/i18n";
-import vuetify from "./plugins/vuetify";
-import { withVuetifyTheme } from "./withVuetifyTheme.decorator";
+import i18n, { i18nConfig } from './plugins/i18n';
+import vuetify from './plugins/vuetify';
+import { withVuetifyTheme } from './withVuetifyTheme.decorator';
 
 const preview: Preview = {
   parameters: {
@@ -20,18 +18,17 @@ const preview: Preview = {
     },
   },
 
-  tags: ["autodocs"],
+  tags: ['autodocs'],
 };
 
 setup((app) => {
   // Registers your app's plugins into Storybook
   app.use(vuetify);
   app.use(i18n);
-  app.use(VueMonacoEditorPlugin);
 });
 
 initialize({
-  onUnhandledRequest: "bypass", // 👈 suppresses the warning
+  onUnhandledRequest: 'bypass', // 👈 suppresses the warning
 });
 
 export const loaders = [mswLoader];
@@ -39,26 +36,26 @@ export const decorators = [withVuetifyTheme];
 
 export const globalTypes = {
   locale: {
-    name: "Locale",
-    description: "Internationalization locale",
+    name: 'Locale',
+    description: 'Internationalization locale',
     defaultValue: i18nConfig.locale,
     toolbar: {
-      icon: "globe",
+      icon: 'globe',
       items: [
-        { value: "en", left: "🇺🇸", title: "English" },
-        { value: "pl", left: "🇵🇱", title: "Polski" },
+        { value: 'en', left: '🇺🇸', title: 'English' },
+        { value: 'pl', left: '🇵🇱', title: 'Polski' },
       ],
       dynamicTitle: true,
     },
   },
   theme: {
-    name: "Theme",
-    description: "Global theme for components",
+    name: 'Theme',
+    description: 'Global theme for components',
     toolbar: {
-      icon: "paintbrush",
+      icon: 'paintbrush',
       items: [
-        { value: "light", title: "Light", left: "🌞" },
-        { value: "dark", title: "Dark", left: "🌛" },
+        { value: 'light', title: 'Light', left: '🌞' },
+        { value: 'dark', title: 'Dark', left: '🌛' },
       ],
       dynamicTitle: true,
     },
